874ff3dfb5aeef00ca212fb4a4676b312a109e02
developfor <will@developfor.com>
2014-03-05 15:01:39 -0500

    json ajax working

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -15,13 +15,17 @@ class SearchController < ApplicationController
 	 	
 	 	 @array = []
 @geowithin = VacantBuilding.collection.find( { :coordinates => { "$geoWithin" =>
-	{ "$center" => [ @vb.coordinates, 0.02 ] }
+	{ "$center" => [ @vb.coordinates, 0.03 ] }
 	} } ).each do |document|
 	 @array << document
 
 end	
 	@arr =  @array.count
-
+respond_to do |format|
+  format.html # show.html.erb
+  format.xml  { render :xml => @array }
+  format.json { render :json => @array }
+end
 	  rescue
 	
 	  end
