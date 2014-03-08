76da708435b7453cba128e4580bf1fe45a12fbeb
developfor <will@developfor.com>
2014-03-08 18:42:23 -0500

    clean up

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,43 +1,38 @@
 class SearchController < ApplicationController
 	
-  def index
-	  begin
-	  	@q = params[:vacant_building][:fullAddress].upcase
-	  rescue
+	def index
+		begin
+			@q = params[:vacant_building][:fullAddress].upcase
+		rescue
 
-	  end
+		end
 
-	  begin
-	  	 @vb = VacantBuilding.find_by(:fullAddress => @q)
-	  	 # @inspect = @vb.inspect
-	  	 # @coordinates = @vb.coordinates
-	  	 # @fullAddress = @vb.fullAddress
-	 	
-	 	 @array = []
-@geowithin = VacantBuilding.collection.find( { :coordinates => { "$geoWithin" =>
-	{ "$center" => [ @vb.coordinates, 0.02 ] }
-	} } ).each do |document|
-	 @array << document
+		begin
+			@vb = VacantBuilding.find_by(:fullAddress => @q)
+			# @inspect = @vb.inspect
+			# @coordinates = @vb.coordinates
+			# @fullAddress = @vb.fullAddress
 
-end	
-	@arr =  @array.count
-respond_to do |format|
-  format.html # show.html.erb
-  format.xml  { render :xml => @array }
-  format.json { render :json => @array }
-end
-	  rescue
-	
-	  end
-	 
-  end
-
-def howdy
-	respond_to do |format|
-	  format.html # show.html.erb
-	  format.xml  { render :xml => VacantBuilding.all }
-	  format.json { render :json => VacantBuilding.all }
+			@array = []
+			@geowithin = VacantBuilding.collection.find( { :coordinates => { "$geoWithin" => { "$center" => [ @vb.coordinates, 0.02 ] } } } )
+			.each do |document|
+				@array << document
+			end	
+			@arr =  @array.count
+			respond_to do |format|
+			  format.html # show.html.erb
+			  format.xml  { render :xml => @array }
+			  format.json { render :json => @array }
+			end
+		rescue
+		end
 	end
-end
 
+	def howdy
+		respond_to do |format|
+		  format.html # show.html.erb
+		  format.xml  { render :xml => VacantBuilding.all }
+		  format.json { render :json => VacantBuilding.all }
+		end
+	end
 end
