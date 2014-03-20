8d1104f12e6dc5a015af6a3e6c1068de307c3d97
developfor <will@developfor.com>
2014-03-20 16:12:43 -0400

    clean up

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -159,7 +159,7 @@ var geojsonFeature;
 
 
 // $.getJSON('/search.json?utf8=%E2%9C%93&vacant_building%5Blat%5D=-76.6331659571&vacant_building%5Blog%5D=39.2973522251', function(result) {
-$.getJSON('/search//search/load_markers.json?utf8=%E2%9C%93&vacant_building%5Blat%5D=-76.6331659571&vacant_building%5Blog%5D=39.2973522251', function(result) {
+$.getJSON('/search/load_markers.json?utf8=%E2%9C%93&vacant_building%5Blat%5D=-76.6331659571&vacant_building%5Blog%5D=39.2973522251', function(result) {
 
 	// preloader end
 	// $(".spinner-loader").hide();
