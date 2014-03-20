553624c332ed063c242513e9bfde1fbdbaaa6909
developfor <will@developfor.com>
2014-03-20 13:14:24 -0400

    wrong

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -17,7 +17,7 @@ class SearchController < ApplicationController
 			 
 			# VacantBuilding.limit(50).geo_near(@vb_coor).spherical
 			# @geowithin = VacantBuilding.collection.find( { :coordinates => { "$geoWithin" => { "$center" => [ @vb_coor, 5 ] } } } ).limit(200)
-			VacantBuilding.limit(200).geo_near(@vb_coor)
+			VacantBuilding.limit(20).geo_near(@vb_coor)
 			.each do |document|
 				@array << document
 			end	
