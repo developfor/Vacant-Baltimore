df15be09d0ab73b6bf6cde1744c2041423c7dedc
developfor <will@developfor.com>
2014-03-20 15:30:26 -0400

    i dunno

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,38 +1,38 @@
 class SearchController < ApplicationController
 	def index
 
-		begin
-			@lat = params[:vacant_building][:lat]	   
-		    @log = params[:vacant_building][:log]
-		    @vb_coor = [ @lat.to_f, @log.to_f]
+		# begin
+		# 	@lat = params[:vacant_building][:lat]	   
+		#     @log = params[:vacant_building][:log]
+		#     @vb_coor = [ @lat.to_f, @log.to_f]
 		    
-		rescue
-		end
+		# rescue
+		# end
 
-		begin
-		    # @vb = VacantBuilding.find_by(coordinates: [-76.5677218731, 39.2919877174])
+		# begin
+		#     # @vb = VacantBuilding.find_by(coordinates: [-76.5677218731, 39.2919877174])
 
 
-			@array = []
+		# 	@array = []
 			 
-			# VacantBuilding.limit(50).geo_near(@vb_coor).spherical
-			# @geowithin = VacantBuilding.collection.find( { :coordinates => { "$geoWithin" => { "$center" => [ @vb_coor, 5 ] } } } ).limit(200)
-			VacantBuilding.limit(20).geo_near(@vb_coor)
-			.each do |document|
-				@array << document
-			end	
+		# 	# VacantBuilding.limit(50).geo_near(@vb_coor).spherical
+		# 	# @geowithin = VacantBuilding.collection.find( { :coordinates => { "$geoWithin" => { "$center" => [ @vb_coor, 5 ] } } } ).limit(200)
+		# 	VacantBuilding.limit(20).geo_near(@vb_coor)
+		# 	.each do |document|
+		# 		@array << document
+		# 	end	
 
-			# puts @array.count
+		# 	# puts @array.count
 
-			respond_to do |format|
-			  format.html
-			  format.xml  { render :xml => @array }
-			  format.js { render :json => @array }
+		# 	respond_to do |format|
+		# 	  format.html
+		# 	  format.xml  { render :xml => @array }
+		# 	  format.js { render :json => @array }
 
-			end
-			# render json: move_json 
-		rescue
-		end
+		# 	end
+		# 	# render json: move_json 
+		# rescue
+		# end
 	end
 	# def desktop
 
@@ -89,7 +89,7 @@ class SearchController < ApplicationController
 			@lat = params[:vacant_building][:lat]	   
 		    @log = params[:vacant_building][:log]
 		    @vb_coor = [ @lat.to_f, @log.to_f]    
-		
+			puts @vb_coor
 
 		
 		    # @vb = VacantBuilding.find_by(coordinates: [-76.5677218731, 39.2919877174])
