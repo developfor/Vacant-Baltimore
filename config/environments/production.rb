c80ac309c9d26977448c09f6c423ca32883c549b
developfor <will@developfor.com>
2014-03-20 00:01:41 -0400

    Revert "rm config ru"
    This reverts commit 0dfba0ecca02a5cd8e279e497dc293cae99b3566.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,61 +1,85 @@
 Vb::Application.configure do
-  # Settings specified here will take precedence over those in config/application.rb
+  config.cache_classes = false
 
-  # Code is not reloaded between requests
-  config.cache_classes = true
+  # Log error messages when you accidentally call methods on nil.
+  config.whiny_nils = true
 
-  # Full error reports are disabled and caching is turned on
-  config.consider_all_requests_local       = false
-  config.action_controller.perform_caching = true
+  # Show full error reports and disable caching
+  config.consider_all_requests_local       = true
+  config.action_controller.perform_caching = false
 
-  # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+  # Don't care if the mailer can't send
+  config.action_mailer.raise_delivery_errors = false
 
-  # Compress JavaScripts and CSS
+  # Print deprecation notices to the Rails logger
+  config.active_support.deprecation = :log
+
+  # Only use best-standards-support built into browsers
+  config.action_dispatch.best_standards_support = :builtin
+
+
+  # Do not compress assets
   config.assets.compress = true
 
-  # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = true
+  # Expands the lines which load the assets
+  config.assets.debug = true
+  # # Settings specified here will take precedence over those in config/application.rb
 
-  # Generate digests for assets URLs
-  config.assets.digest = true
+  # # Code is not reloaded between requests
+  # config.cache_classes = true
 
-  # Defaults to nil and saved in location specified by config.assets.prefix
-  # config.assets.manifest = YOUR_PATH
+  # # Full error reports are disabled and caching is turned on
+  # config.consider_all_requests_local       = false
+  # config.action_controller.perform_caching = true
 
-  # Specifies the header that your server uses for sending files
-  # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
-  # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
+  # # Disable Rails's static asset server (Apache or nginx will already do this)
+  # config.serve_static_assets = false
 
-  # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  # # Compress JavaScripts and CSS
+  # config.assets.compress = true
 
-  # See everything in the log (default is :info)
-  # config.log_level = :debug
+  # # Don't fallback to assets pipeline if a precompiled asset is missed
+  # config.assets.compile = true
 
-  # Prepend all log lines with the following tags
-  # config.log_tags = [ :subdomain, :uuid ]
+  # # Generate digests for assets URLs
+  # config.assets.digest = true
 
-  # Use a different logger for distributed setups
-  # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
+  # # Defaults to nil and saved in location specified by config.assets.prefix
+  # # config.assets.manifest = YOUR_PATH
 
-  # Use a different cache store in production
-  # config.cache_store = :mem_cache_store
+  # # Specifies the header that your server uses for sending files
+  # # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
+  # # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
-  # Enable serving of images, stylesheets, and JavaScripts from an asset server
-  # config.action_controller.asset_host = "http://assets.example.com"
+  # # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
+  # # config.force_ssl = true
 
-  # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( search.js )
+  # # See everything in the log (default is :info)
+  # # config.log_level = :debug
 
-  # Disable delivery errors, bad email addresses will be ignored
-  # config.action_mailer.raise_delivery_errors = false
+  # # Prepend all log lines with the following tags
+  # # config.log_tags = [ :subdomain, :uuid ]
 
-  # Enable threaded mode
-  # config.threadsafe!
+  # # Use a different logger for distributed setups
+  # # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
 
-  # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
-  # the I18n.default_locale when a translation can not be found)
+  # # Use a different cache store in production
+  # # config.cache_store = :mem_cache_store
+
+  # # Enable serving of images, stylesheets, and JavaScripts from an asset server
+  # # config.action_controller.asset_host = "http://assets.example.com"
+
+  # # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
+  # # config.assets.precompile += %w( search.js )
+
+  # # Disable delivery errors, bad email addresses will be ignored
+  # # config.action_mailer.raise_delivery_errors = false
+
+  # # Enable threaded mode
+  # # config.threadsafe!
+
+  # # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
+  # # the I18n.default_locale when a translation can not be found)
   config.i18n.fallbacks = true
 
   # Send deprecation notices to registered listeners
