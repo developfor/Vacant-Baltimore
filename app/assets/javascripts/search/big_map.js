41b003fb213d3a6b6bc53fbbcc4893c9ebfbd7b0
developfor <will@developfor.com>
2014-03-26 09:27:41 -0400

    touch punch last

--- app/assets/javascripts/search/big_map.js
+++ app/assets/javascripts/search/big_map.js
@@ -3,7 +3,6 @@
 // = require jquery_ujs
 // = require leaflet/leaflet
 // = require leaflet.markercluster-src
-// = require jquery.ui.touch-punch.min
 // = require jquery.nicescroll
 
 
@@ -11,3 +10,5 @@
 // = require search/contents/main
 // = require search/contents/search_big_map
 
+//touch punch last
+// = require jquery.ui.touch-punch.min
--- app/assets/javascripts/search/index.js
+++ app/assets/javascripts/search/index.js
@@ -3,11 +3,13 @@
 // = require jquery_ujs
 // = require leaflet/leaflet
 // = require leaflet.markercluster-src
-// = require jquery.ui.touch-punch.min
 // = require jquery.nicescroll
 
 
 // assets files
 // = require search/contents/main
 // = require search/contents/search
+
+//touch punch last
+// = require jquery.ui.touch-punch.min
 
